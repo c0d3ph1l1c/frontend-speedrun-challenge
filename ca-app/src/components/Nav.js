e75c4016cea2a825bc38fe5a079a600fde5a21ee
c0d3ph1l1c <kwl.c0d3r@gmail.com>
2020-09-02 18:08:37 +0800

    Make navbar responsive

--- ca-app/src/components/Nav.js
+++ ca-app/src/components/Nav.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
-import '../static/css/components/nav.scss';
+import '../static/css/components/Nav.scss';
 
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navExpanded: false,
+      navHeight: 'auto'
+    };
+    this.navRef = React.createRef();
+  }
+
+  toggleNav = () => {
+    this.setState(prevState => ({ navExpanded: !prevState.navExpanded }));
+  }
+
   render() {
     return (
       <nav className="navbar">
         <div className="container-fluid">
           <div className="row">
-            <h1 className="navbar-brand col-lg-2">
-              <a href="#">Ca.</a>
-            </h1>
-            <div className="navbar-toggle">
-              <div className="bar"></div>
+            <div className="col-11 col-lg-2 ml-auto mr-auto">
+              <div className="row h-100 mt-auto mb-auto">
+                <h1 className="navbar-brand mr-auto">
+                  <a href="#">Ca.</a>
+                </h1>
+                <button 
+                  className="navbar-toggle d-block d-lg-none" 
+                  onClick={this.toggleNav}
+                >
+                  <div className="center"></div>
+                </button>
+              </div>
             </div>
-            <div className="navbar-collapse col-lg-10">
-              <div className="container-fluid"> 
+            <div 
+              ref={this.navRef} 
+              className={`navbar-collapse ml-auto mr-auto col-11 col-lg-10 ${this.state.navExpanded && 'show'}`}
+              style={{ height: this.state.navHeight }}
+            >
+              <div className="container-fluid nav-wrapper"> 
                 <div className="row">
-                  <ul className="navbar-nav col-lg-9 ml-auto">
+                  <ul className="navbar-nav col-12 col-lg-9 ml-auto">
                     <li className="nav-item">
                       <a href="#" className="nav-link">Home</a>
                     </li>
