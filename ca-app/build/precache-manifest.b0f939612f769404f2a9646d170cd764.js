6e48b23ef968031d39bd4db12640302971de4be9
c0d3ph1l1c <kwl.c0d3r@gmail.com>
2020-10-01 13:10:39 +0800

    Fix path in ca-app build-version for github page

--- ca-app/build/precache-manifest.b0f939612f769404f2a9646d170cd764.js
+++ ca-app/build/precache-manifest.b0f939612f769404f2a9646d170cd764.js
@@ -29,118 +29,118 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "b3f61f04ee9eec8f681664af2458ddc1",
-    "url": "/static/media/app-1.b3f61f04.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/app-1.b3f61f04.jpg"
   },
   {
     "revision": "c951670148c05b92657fd2595b02b09a",
-    "url": "/static/media/app-2.c9516701.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/app-2.c9516701.jpg"
   },
   {
     "revision": "d82852974093c7e3748f43fc9d0f64c7",
-    "url": "/static/media/app-3.d8285297.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/app-3.d8285297.jpg"
   },
   {
     "revision": "dab1896133587fdee9fcda8b0509c648",
-    "url": "/static/media/app-4.dab18961.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/app-4.dab18961.jpg"
   },
   {
     "revision": "7a1e342ef75462130d1d57c32614262f",
-    "url": "/static/media/app-5.7a1e342e.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/app-5.7a1e342e.jpg"
   },
   {
     "revision": "1961205e294ed5a4163b76cafaff6672",
-    "url": "/static/media/client-1.1961205e.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/client-1.1961205e.jpg"
   },
   {
     "revision": "6af7bbca2494289ecd5037f01920b0f2",
-    "url": "/static/media/client-2.6af7bbca.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/client-2.6af7bbca.jpg"
   },
   {
     "revision": "86439e15b025ab70540b5d3cec262508",
-    "url": "/static/media/client-3.86439e15.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/client-3.86439e15.jpg"
   },
   {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
-    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
     "revision": "05acfdb568b3df49ad31355b19495d4a",
-    "url": "/static/media/ionicons.05acfdb5.woff"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/ionicons.05acfdb5.woff"
   },
   {
     "revision": "24712f6c47821394fba7942fbb52c3b2",
-    "url": "/static/media/ionicons.24712f6c.ttf"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/ionicons.24712f6c.ttf"
   },
   {
     "revision": "2c2ae068be3b089e0a5b59abb1831550",
-    "url": "/static/media/ionicons.2c2ae068.eot"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/ionicons.2c2ae068.eot"
   },
   {
     "revision": "c037dbbc0e6790f30e824a50010df5fb",
-    "url": "/static/media/ionicons.c037dbbc.svg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/ionicons.c037dbbc.svg"
   },
   {
     "revision": "69cad956313301082bb20492429cad1f",
-    "url": "/static/media/special.69cad956.png"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/special.69cad956.png"
   },
   {
     "revision": "83974ecf219fbb604f9a4b4d4a0bbdab",
-    "url": "/static/media/team-1.83974ecf.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/team-1.83974ecf.jpg"
   },
   {
     "revision": "c4c61ad0e90d305c64e45b0cbbeb8ec0",
-    "url": "/static/media/team-2.c4c61ad0.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/team-2.c4c61ad0.jpg"
   },
   {
     "revision": "cead6621f0e9bde1579e757124a006fd",
-    "url": "/static/media/team-3.cead6621.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/team-3.cead6621.jpg"
   },
   {
     "revision": "59c6c2376e745757b5eb64883429cfa5",
-    "url": "/static/media/team-4.59c6c237.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/team-4.59c6c237.jpg"
   },
   {
     "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
-    "url": "/static/media/themify.2c454669.eot"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/themify.2c454669.eot"
   },
   {
     "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
-    "url": "/static/media/themify.a1ecc3b8.woff"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/themify.a1ecc3b8.woff"
   },
   {
     "revision": "cba68f986e60df8c74f4a53c3e39595c",
-    "url": "/static/media/themify.cba68f98.svg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/themify.cba68f98.svg"
   },
   {
     "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
-    "url": "/static/media/themify.e23a7dca.ttf"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/themify.e23a7dca.ttf"
   },
   {
     "revision": "364f5bc5fe0c237ed8327f6e54789192",
-    "url": "/static/media/video.364f5bc5.jpg"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/video.364f5bc5.jpg"
   },
   {
     "revision": "0827b8ae681dda62617eb5d02d5fee40",
-    "url": "/static/media/welcome-bg.0827b8ae.webp"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/welcome-bg.0827b8ae.webp"
   },
   {
     "revision": "7bb1972fbb2be1e82feea52f358e4119",
-    "url": "/static/media/welcome-img.7bb1972f.png"
+    "url": "/frontend-speedrun-challenge/ca-app/build/static/media/welcome-img.7bb1972f.png"
   }
 ]);
